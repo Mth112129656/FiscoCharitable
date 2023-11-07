1040495f78f8099fa307f841e6e5edb42bae3f59
mth <1121229656@qq.com>
2023-11-07 17:43:23 +0800

    配置swagger和knife4j

--- src/main/java/org/example/demo/controller/UserController.java
+++ src/main/java/org/example/demo/controller/UserController.java
@@ -1,51 +1,69 @@
 package org.example.demo.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.demo.entity.User;
 import org.example.demo.service.UserService;
-import org.example.demo.util.FiscoBcos;
+
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "用户信息查询（测试）")
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
-    @Autowired
-    private FiscoBcos fiscoBcos;
 
-    @RequestMapping("/testgetcontract")
+
+    @Operation(summary = "区块链数据查询")
+    @GetMapping("/testgetcontract")
     String testGetContract() throws Exception {
 
         return userService.GetContract();
     }
 
-    @RequestMapping("/testsetcontract/{value}")
-    String testSetContract(String value) throws Exception {
+    @Operation(summary = "区块链修改数据")
+    @Parameters({
+            @Parameter(name = "value",description = "修改名称",in = ParameterIn.PATH),
+    })
+    @PostMapping("/testsetcontract/{value}")
+    String testSetContract(@PathVariable("value") String value) throws Exception {
         return userService.SetContract(value);
     }
 
-    @RequestMapping("/userall")
+    @Operation(summary = "查询全部数据（数据库）")
+    @GetMapping("/userall")
     List<User> userAll() {
         List<User> user = userService.userAll();
         return user;
     }
 
-    @RequestMapping("/userone/{id}")
-    User userOne(Integer id) {
+    @Operation(summary = "根据id查询用户信息")
+    @Parameters({
+            @Parameter(name = "id",description = "查询id",in = ParameterIn.PATH),
+    })
+    @GetMapping("/userone/{id}")
+    User userOne(@PathVariable("id") Integer id) {
         User user = userService.userOne(id);
         return user;
     }
 
 
-    @RequestMapping("/username/{name}")
-    User userName(String name) {
+    @Operation(summary = "根据姓名查询用户信息")
+    @Parameters({
+            @Parameter(name = "name",description = "name",in = ParameterIn.PATH),
+    })
+    @GetMapping("/username/{name}")
+    User userName(@PathVariable("name") String name) {
         User user = userService.userName(name);
         return user;
     }
--- src/main/java/org/example/demo/entity/User.java
+++ src/main/java/org/example/demo/entity/User.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Data
